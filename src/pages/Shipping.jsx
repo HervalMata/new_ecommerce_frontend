e8db8954a0a58cba80c525600c2bae96688bbccb
Herval Mata <hervalmataamparo@gmail.com>
2026-08-24 12:35:51 -0300

    :sparkles: feat: new ecommerce frontend - setup shipping page

---
+++ src/pages/Shipping.jsx
@@ -0,0 +1,236 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import {Link} from "react-router-dom";
+import {IoIosArrowForward} from "react-icons/io";
+import {useState} from "react";
+
+const Shipping = () => {
+    const [res, setRes] = useState(false);
+    const [state, setState] = useState({
+        name: '',
+        address: '',
+        phone: '',
+        post: '',
+        province: '',
+        city: '',
+        area: '',
+    });
+
+    const inputHandle = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const save = (e) => {
+        e.preventDefault();
+        const { name, address, phone, post, province, city, area} = state
+        if (name && address && phone && post && province && city && area) {
+            setRes(true);
+        }
+    }
+
+    return (
+        <div>
+            <Header />
+            <section className='bg-[url("http://localhost:3000/images/banner/shop.png")] h-[220px] mt-6 bg-cover
+                                bg-no-repeat relative bg-left'>
+                <div className='absolute left-0 top-0 w-full h-full bg-[#2422228A]'>
+                    <div className='w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto'>
+                        <div className="flex flex-col items-center justify-center gap-1 h-full text-white">
+                            <h2 className="text-3xl font-bold">Entregas</h2>
+                          <div>
+                              <Link to=''>Home</Link>
+                              <span className="pt-1"><IoIosArrowForward/></span>
+                              <span>Entregas</span>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="bg-[#EEEEEE]">
+                <div className='w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto py-16'>
+                    <div className="w-full flex flex-wrap">
+                        <div className="md-lg:w-full w-[67%]">
+                            <div className="flex flex-col gap-3">
+                                <div className="bg-white p-6 shadow-sm rounded-md">
+                                    <h2 className="text-slate-600 font-bold pb-3"> Informações da Entrega </h2>
+
+                                    {
+                                        !res &&
+                                            <>
+                                                <form onSubmit={save}>
+                                                    <div className="flex md:flex-col md:gap-2 gap-5 w-full text-slate-600">
+                                                        <div className="flex flex-col gap-1 mb-2 w-full">
+                                                            <label htmlFor="name"> Nome </label>
+                                                            <input onChange={inputHandle} value={state.name}
+                                                                className="w-full px-3 py-2 border border-slate-200 outline-none
+                                                             focus:border-green-500 rounded-md"
+                                                                   type="text" name="name" id="name" placeholder="Nome" />
+                                                        </div>
+
+                                                        <div className="flex flex-col gap-1 mb-2 w-full">
+                                                            <label htmlFor="address"> Enderêço </label>
+                                                            <input onChange={inputHandle} value={state.address}
+                                                                className="w-full px-3 py-2 border border-slate-200 outline-none
+                                                             focus:border-green-500 rounded-md"
+                                                                   type="text" name="address" id="address" placeholder="Enderêço" />
+                                                        </div>
+                                                    </div>
+
+                                                    <div className="flex md:flex-col md:gap-2 gap-5 w-full text-slate-600">
+                                                        <div className="flex flex-col gap-1 mb-2 w-full">
+                                                            <label htmlFor="phone"> Celular </label>
+                                                            <input onChange={inputHandle} value={state.phone}
+                                                                className="w-full px-3 py-2 border border-slate-200 outline-none
+                                                             focus:border-green-500 rounded-md"
+                                                                   type="text" name="phone" id="phone" placeholder="Celular" />
+                                                        </div>
+
+                                                        <div className="flex flex-col gap-1 mb-2 w-full">
+                                                            <label htmlFor="post"> CEP </label>
+                                                            <input onChange={inputHandle} value={state.post}
+                                                                className="w-full px-3 py-2 border border-slate-200 outline-none
+                                                             focus:border-green-500 rounded-md"
+                                                                   type="text" name="post" id="post" placeholder="CEP" />
+                                                        </div>
+                                                    </div>
+
+                                                    <div className="flex md:flex-col md:gap-2 gap-5 w-full text-slate-600">
+                                                        <div className="flex flex-col gap-1 mb-2 w-full">
+                                                            <label htmlFor="province"> Bairro </label>
+                                                            <input onChange={inputHandle} value={state.province}
+                                                                className="w-full px-3 py-2 border border-slate-200 outline-none
+                                                             focus:border-green-500 rounded-md"
+                                                                   type="text" name="province" id="province" placeholder="Bairro" />
+                                                        </div>
+
+                                                        <div className="flex flex-col gap-1 mb-2 w-full">
+                                                            <label htmlFor="city"> Cidade </label>
+                                                            <input onChange={inputHandle} value={state.city}
+                                                                className="w-full px-3 py-2 border border-slate-200 outline-none
+                                                             focus:border-green-500 rounded-md"
+                                                                   type="text" name="city" id="city" placeholder="Cidade" />
+                                                        </div>
+                                                    </div>
+
+                                                    <div className="flex md:flex-col md:gap-2 gap-5 w-full text-slate-600">
+                                                        <div className="flex flex-col gap-1 mb-2 w-full">
+                                                            <label htmlFor="area"> Area </label>
+                                                            <input onChange={inputHandle} value={state.area}
+                                                                className="w-full px-3 py-2 border border-slate-200 outline-none
+                                                             focus:border-green-500 rounded-md"
+                                                                   type="text" name="area" id="area" placeholder="Area" />
+                                                        </div>
+
+                                                        <div className="flex flex-col gap-1 mt-7 mb-2 w-full">
+                                                            <button className="px-3 py-[9px] rounded-sm hover:shadow-green-500/50
+                                                                   hover:shadow-lg bg-green-500 text-white">
+                                                                Salvar Alterações
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </>
+                                    }
+
+                                    {
+                                        res &&
+                                        <div className="flex flex-col gap-3">
+                                            <h2 className="text-slate-600 font-semibold pb-2">Entregar Para: </h2>
+                                            <p>
+                                                <span className="bg-blue-200 text-blue-800 text-sm font-medium mr-2 px-2 py-1">Home</span>
+                                                <span>{state.phone}, {state.address}, {state.province} {state.city} {state.area} </span>
+                                                <span onClick={() => setRes(false)}
+                                                      className="text-indigo-500 cursor-pointer">
+                                                    Mudar
+                                                </span>
+                                            </p>
+                                            <p className="text-slate-600 text-sm">Email Para john@gmail.com</p>
+                                        </div>
+                                    }
+                                </div>
+
+                                {
+                                    [1,2].map((p, i) =>
+                                        <div className="flex bg-white p-4 flex-col gap-2">
+                                            <div className="flex justify-start items-center">
+                                                <h2 className="test-md text-slate-600 font-bold">Cris Laços</h2>
+                                            </div>
+                                            {
+                                                [1,2].map((p, i) =>
+                                                    <div className="w-full flex flex-wrap">
+                                                        <div className="flex sm:w-full gap-2 w-7/12">
+                                                            <div className="flex gap-2 justify-start items-center">
+                                                                <img className="w-[80px] h-[80px]"
+                                                                    src={`http://localhost:3000/images/products/${i+1}.webp`} alt="" />
+                                                                <div className="pr-4 text-slate-600">
+                                                                    <h2 className="text-md font-semibold">Nome do Produto</h2>
+                                                                    <span className="text-sm">Marca: Cris Laços</span>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+
+                                                        <div className="flex justify-between w-5/12 sm:w-full sm:mt-3">
+                                                            <div className="pl-4 sm:pl-0">
+                                                                <h2 className="text-lg text-orange-500">R$ 240,00</h2>
+                                                                <p className="line-through">R$ 300,00</p>
+                                                                <p>-15%</p>
+                                                            </div>
+                                                            <div className="flex gap-2 flex-col">
+                                                                <div className="flex bg-slate-200 h-[30px]
+                                                                           justify-center items-center text-xl">
+                                                                    <div className="px-3 cursor-pointer">-</div>
+                                                                    <div className="px-3">2</div>
+                                                                    <div className="px-3 cursor-pointer">+</div>
+                                                                </div>
+                                                                <button className="px-5 py-[3px] bg-red-500 text-white"
+                                                                >Remover</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>)
+                                            }
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        </div>
+
+                        <div className="w-[33%] md-lg:w-full">
+                            <div className="pl-3 md-lg:pl-0 md-lg:mt-5">
+                                <div className="bg-white p-3 text-slate-600 flex flex-col gap-3">
+                                    <h2 className="text-xl font-bold">Resumo da Ordem</h2>
+                                    <div className="flex justify-between items-center">
+                                        <span>Total de itens (5)</span>
+                                        <span>R$ 343,00</span>
+                                    </div>
+                                    <div className="flex justify-between items-center">
+                                        <span>Taxa de Entrega</span>
+                                        <span>R$ 40,00</span>
+                                    </div>
+                                    <div className="flex justify-between items-center">
+                                        <span>Total Para pagamento</span>
+                                        <span>R$ 450,00</span>
+                                    </div>
+                                    <div className="flex justify-between items-center">
+                                        <span>Total</span>
+                                        <span className="text-lg text-[#059473]">R$ 490,00</span>
+                                    </div>
+                                    <button disabled={!!res}
+                                        className={`px-5 py-[6px] rounded-sm hover:shadow-green-500/50 hover:shadow-lg ${
+                                            res ? 'bg-red-500' : 'bg-red-300'
+                                        } text-sm text-white uppercase`}>
+                                    Finalizar Ordem</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </div>
+    )
+}
+
+export default Shipping
